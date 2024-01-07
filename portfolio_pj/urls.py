e340fb8aeaa06a8ba99028d9ff776555d0593fad
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-01-07 16:59:53 +0900

    update urls.py

--- portfolio_pj/urls.py
+++ portfolio_pj/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from . import views
+from portfolio_app import views as portfolio_app_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
     path('api/v1/projects/', include('portfolio_app.urls')),
-    path('', views.redirect_to_documentation),
+    path('', portfolio_app_views.AllProjectsView.as_view()), 
 ]
--- portfolio_pj/views.py
+++
@@ -1,4 +0,0 @@
-from django.http import HttpResponseRedirect
-
-def redirect_to_documentation(request):
-    return HttpResponseRedirect('/api/documentation/')

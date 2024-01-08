7746c7b4a456be0612d1b301cf7f03d9450b3297
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-01-08 15:05:55 +0900

    update views

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -2,7 +2,6 @@ from rest_framework import status, authentication, permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .forms import ProjectForm
 from .models import Project, Skill
 from .serializers import ProjectSerializer, ProjectDetailSerializer, SkillSerializer
 
@@ -20,47 +19,6 @@ class NewestProjectsView(APIView):
 
         return Response(serializer.data)
     
-class AdminView(APIView):
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get(self, request, format=None):
-        projects = Project.objects.filter(created_by=request.user)
-        serializer = ProjectSerializer(projects, many=True)
-
-        return Response(serializer.data)
-
-class CreateProjectView(APIView):
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
-
-    def post(self, request):
-        form = ProjectForm(request.data)
-
-        if form.is_valid():
-            project = form.save(commit=False)
-            project.created_by = request.user
-            project.save()
-
-            return Response({'status': 'created'})
-        else:
-            return Response({'status': 'errors', 'errors': form.errors})
-    
-    def put(self, request, pk):
-        project = Project.objects.get(pk=pk, created_by=request.user)
-        form = ProjectForm(request.data, instance=project)
-        if form.is_valid():
-            form.save()
-            return Response({'status': 'updated'})
-        else:
-            return Response({'status': 'errors', 'errors': form.errors}, status=400)
-
-    def delete(self, request, pk):
-        project = Project.objects.get(pk=pk, created_by=request.user)
-        project.delete()
-
-        return Response({'status': 'deleted'})
-
 class AllProjectsView(APIView):
     def get(self, request, format=None):
         projects = Project.objects.all()

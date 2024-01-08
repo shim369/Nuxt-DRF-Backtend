4c08ae4c21eaa29e3cbf8a5662316c98549121e5
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-01-08 15:06:05 +0900

    update urls

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -5,10 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.AllProjectsView.as_view()),
     path('skills/', views.SkillsView.as_view()),
-    path('admin/', views.AdminView.as_view()),
-    path('create/', views.CreateProjectView.as_view()),
     path('newest/', views.NewestProjectsView.as_view()),
     path('<int:pk>/', views.ProjectsDetailView.as_view()),
-    path('<int:pk>/delete/', views.CreateProjectView.as_view()),
-    path('<int:pk>/edit/', views.CreateProjectView.as_view()),
 ]

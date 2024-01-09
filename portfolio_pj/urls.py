74dd5bbf3450715d60f9e9b31e67bfa4f6408f35
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-01-09 16:42:20 +0900

    update media path

--- portfolio_pj/urls.py
+++ portfolio_pj/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from portfolio_app import views as portfolio_app_views
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('djoser.urls')),
     path('api/v1/', include('djoser.urls.authtoken')),
     path('api/v1/projects/', include('portfolio_app.urls')),
-    path('', portfolio_app_views.AllProjectsView.as_view()), 
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', portfolio_app_views.AllProjectsView.as_view()),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]

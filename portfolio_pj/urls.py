122b4aaac41a038b0fd5a748efa0554034d7ea44
Shimpei Ohtani <ohtani.shimpei@gmail.com>
2024-01-09 15:48:12 +0900

    update urls.py

--- portfolio_pj/urls.py
+++ portfolio_pj/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from portfolio_app import views as portfolio_app_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +10,4 @@ urlpatterns = [
     path('api/v1/', include('djoser.urls.authtoken')),
     path('api/v1/projects/', include('portfolio_app.urls')),
     path('', portfolio_app_views.AllProjectsView.as_view()), 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
